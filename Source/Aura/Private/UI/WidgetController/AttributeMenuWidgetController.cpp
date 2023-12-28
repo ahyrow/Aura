88cdb02f3b8e4fe324736873011ed6d4b8ce78c4
ahyrow <644619579@qq.com>
2023-12-28 18:00:19 +0800

    P91

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,6 +3,10 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependcies()
 {
 	
@@ -10,7 +14,16 @@ void UAttributeMenuWidgetController::BindCallbacksToDependcies()
 
 void UAttributeMenuWidgetController::BroadcastInitiaValues()
 {
-    //绑定在AuraInfo中的属性结构
+    
+	//先拿到Aura的属性集
+	UAuraAttributeSet* AS = Cast<UAuraAttributeSet>(AttributeSet);
+	//检查蓝图中的数据资产是否被赋值
+	check(AttributeInfo); //声明的数据资产指针由蓝图赋值
 
-	
+	//调用数据资产信息类中的查找标签函数 会返回一个信息的结构体 
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInforTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	//通过拿到属性集中的力量值给指定的信息进行同步
+	Info.AttributeValue = AS->GetStrength();
+	//进行广播
+	AttributeInfoDelegate.Broadcast(Info);
 }
