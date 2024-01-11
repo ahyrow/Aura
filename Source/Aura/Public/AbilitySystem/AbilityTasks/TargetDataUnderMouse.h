5edf0523c8eb7cdc6c1f9dc365e0b9d5a37bf53d
ahyrow <644619579@qq.com>
2024-01-11 17:50:49 +0800

    GAS P112 Ing

---
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
